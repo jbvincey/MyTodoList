fe5343ee74f5f2f47aae05adf5c2509abad0cef5
jbvincey <jbvincey@deezer.com>
2018-10-28 19:22:56 +0100

    update checkable cell view margins

--- ui/src/main/java/com/jbvincey/ui/recycler/cells/checkablecell/CheckableCellView.kt
+++ ui/src/main/java/com/jbvincey/ui/recycler/cells/checkablecell/CheckableCellView.kt
@@ -4,9 +4,8 @@ import android.content.Context
 import android.util.AttributeSet
 import com.jbvincey.ui.R
 import com.jbvincey.ui.databinding.ViewCheckableCellBinding
-import android.util.TypedValue
 import com.jbvincey.ui.recycler.cells.AbstractCellView
-import kotlinx.android.synthetic.main.view_checkable_cell.view.*
+
 
 /**
  * Created by jbvincey on 14/10/2018.
@@ -16,11 +15,6 @@ class CheckableCellView @JvmOverloads constructor(
         attrs: AttributeSet? = null,
         defStyleAttr: Int = 0
 ) : AbstractCellView<CheckableCellViewModel, ViewCheckableCellBinding>(context, attrs, defStyleAttr) {
-
-    init {
-        val horizontalPadding = context.resources.getDimensionPixelSize(R.dimen.list_margin_horizontal)
-        setPadding(horizontalPadding, 0, horizontalPadding, 0)
-    }
 
     override fun getLayout() = R.layout.view_checkable_cell
 
