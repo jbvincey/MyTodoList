3011a45ec79eb948156d224af27f59ba36b42c04
jbvincey <jbvincey@gmail.com>
2021-02-04 20:18:21 +0100

    update appcompat, material, kotlin, constraint_layout, design and add flipper

--- app/src/main/java/com/jbvincey/mytodolist/MyTodoListApp.kt
+++ app/src/main/java/com/jbvincey/mytodolist/MyTodoListApp.kt
@@ -1,11 +1,19 @@
 package com.jbvincey.mytodolist
 
 import android.app.Application
+import com.facebook.flipper.android.AndroidFlipperClient
+import com.facebook.flipper.android.utils.FlipperUtils
+import com.facebook.flipper.plugins.databases.DatabasesFlipperPlugin
+import com.facebook.flipper.plugins.inspector.DescriptorMapping
+import com.facebook.flipper.plugins.inspector.InspectorFlipperPlugin
+import com.facebook.soloader.SoLoader
 import com.facebook.stetho.Stetho
 import com.jbvincey.core.dependencies.coreModule
 import com.jbvincey.featureaddtodo.featureAddTodoModule
+import com.jbvincey.featureaddtodolist.featureAddListTodoModule
 import com.jbvincey.navigation.navigationModule
 import com.jbvincey.todolist.featureTodoListModule
+import com.jbvincey.todolistpicker.featureTodoListPickerModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidFileProperties
 import org.koin.android.ext.koin.androidLogger
@@ -14,10 +22,14 @@ import org.koin.core.context.startKoin
 /**
  * Created by jbvincey on 16/09/2018.
  */
-class MyTodoListApp: Application() {
+class MyTodoListApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            initializeFlipper()
+        }
 
         startKoin {
             // use AndroidLogger as Koin Logger - default Level.INFO
@@ -31,14 +43,28 @@ class MyTodoListApp: Application() {
 
             // module list
             modules(listOf(
-                    coreModule,
-                    navigationModule,
-                    featureTodoListModule,
-                    featureAddTodoModule
+                coreModule,
+                navigationModule,
+                featureTodoListPickerModule,
+                featureAddListTodoModule,
+                featureTodoListModule,
+                featureAddTodoModule
             ))
         }
 
-        Stetho.initializeWithDefaults(this)
+        if (BuildConfig.DEBUG) {
+            Stetho.initializeWithDefaults(this)
+        }
+    }
+
+    private fun initializeFlipper() {
+        SoLoader.init(this, false)
+        if (FlipperUtils.shouldEnableFlipper(this)) {
+            val client = AndroidFlipperClient.getInstance(this)
+            client.addPlugin(InspectorFlipperPlugin(this, DescriptorMapping.withDefaults()))
+            client.addPlugin(DatabasesFlipperPlugin(this))
+            client.start()
+        }
     }
 
 }
