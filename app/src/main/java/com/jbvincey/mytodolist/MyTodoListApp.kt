c6c03f5a450b7015af66600e9d250c6f7d31bd4b
jbvincey <jbvincey@gmail.com>
2024-06-08 20:23:04 +0200

    fix AppInitializer and remove imports

--- app/src/main/java/com/jbvincey/mytodolist/MyTodoListApp.kt
+++ app/src/main/java/com/jbvincey/mytodolist/MyTodoListApp.kt
@@ -1,13 +1,6 @@
 package com.jbvincey.mytodolist
 
 import android.app.Application
-import com.facebook.flipper.android.AndroidFlipperClient
-import com.facebook.flipper.android.utils.FlipperUtils
-import com.facebook.flipper.plugins.databases.DatabasesFlipperPlugin
-import com.facebook.flipper.plugins.inspector.DescriptorMapping
-import com.facebook.flipper.plugins.inspector.InspectorFlipperPlugin
-import com.facebook.soloader.SoLoader
-import com.facebook.stetho.Stetho
 import com.jbvincey.core.dependencies.coreModule
 import com.jbvincey.featureaddtodo.featureAddTodoModule
 import com.jbvincey.featureaddtodolist.featureAddListTodoModule
--- app/src/release/java/com.jbvincey.mytodolist/AppInitializer.kt
+++ app/src/release/java/com.jbvincey.mytodolist/AppInitializer.kt
@@ -1,13 +1,8 @@
 package com.jbvincey.mytodolist
 
-import com.facebook.flipper.android.AndroidFlipperClient
-import com.facebook.flipper.android.utils.FlipperUtils
-import com.facebook.flipper.plugins.databases.DatabasesFlipperPlugin
-import com.facebook.flipper.plugins.inspector.DescriptorMapping
-import com.facebook.flipper.plugins.inspector.InspectorFlipperPlugin
-import com.facebook.soloader.SoLoader
+import android.content.Context
 
 object AppInitializer {
 
-    fun initializeFlipper() {}
+    fun initializeFlipper(context: Context) {}
 }
